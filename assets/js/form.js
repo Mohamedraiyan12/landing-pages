396762017d62500a83b9578f78aa3f0a00a8cb0e
Mohamedraiyan12 <mmohamedraiyan@gmail.com>
2024-07-12 19:09:40 +0530

    some changes

--- assets/js/form.js
+++ assets/js/form.js
@@ -4,35 +4,51 @@
 
 // var messagesboy = "Name" + Username +
 // "<br/> Email " + email +
-// "<br/> Message " + messages;
-
+// "<br/> Message " + message
+// const form = document.querySelectorAll("form");
+// const username = document.getElementById("name");
+// const emails = document.getElementById("email");
+// const message = document.getElementById("messages");
 
 function emailsend() {
+    // let boymsg = `nam: ${username.value}<br>mail:${emails.value}<br> message: ${message.value}`;
+    let username = document.getElementById("name").value;
+    let email = document.getElementById("email").value;
+    let message = document.getElementById("messages").value;
+
+    let messagebody = "Nam" + username +
+     "<br/> Phon" + message +
+     "<br/> mail" + email;
     Email.send({
         Host : "smtp.elasticemail.com",
         Username : "mohamedraiyan158@gmail.com",
-        Password : "34A35181E5FA74D72247850AFB9DFC8AB66394C1A82C7547DFD5774A260A4EC9AA221ED3BC0698EA9AB1CF5EA45BD192",
-        To : 'mohamedraiyan158@gmail.com',
+        Password : "848042E7C8E5815E06D9EBC1C836411155DA",
+        To : "mohamedraiyan158@gmail.com",
         From :"mohamedraiyan158@gmail.com",
         Subject : "This is the subject",
-        Body : "raiyan"
+        Body : messagebody
     }).then(
       message => alert(message)
     );
 }
-function emailsend() {
-var username = document.getElementById('name').value
-var email = document.getElementById('email').value
-var message = document.getElementById('messages').value
 
-    var parms = {
-        email : email,
-        name : username,
-        message : message
-    };
-    emailjs.send('service_98c88bj','template_riev8ys',parms)
-    .then(function(response) {
-        console.log("success", response.status, response.text)
-        window.alert("sent sucessfully");
-    })
-}
+// form.addEventListener("submit", () => {
+//     emailsend()
+// });
+
+// function emailsend() {
+// var username = document.getElementById('name').value
+// var email = document.getElementById('email').value
+// var message = document.getElementById('messages').value
+
+//     var parms = {
+//         email : email,
+//         name : username,
+//         message : message
+//     };
+//     emailjs.send('service_98c88bj','template_riev8ys',parms)
+//     .then(function(response) {
+//         console.log("success", response.status, response.text)
+//         window.alert("sent sucessfully");
+//     })
+// }
